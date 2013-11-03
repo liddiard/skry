7d5a89e78345c4a9b4a8206c5564b47afc33fbe2
omniaura <omniaura5@gmail.com>
2013-11-02 22:48:36 -0700

    made some Author fields optional, fixed Author unicode method bug

--- main/models.py
+++ main/models.py
@@ -3,16 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, null=True, blank=True)
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
-    organization = models.CharField(max_length=32, default="Daily Bruin")
-    # title = models.CharField(max_length=32)
-    email = models.EmailField()
-    twitter = models.CharField(max_length=15) # current max handle length
-    facebook = models.CharField(max_length=32)
-    mug = models.ImageField(upload_to='mug/%Y')
-    bio = models.TextField()
+    organization = models.CharField(max_length=32, default="Daily Bruin",
+                                    blank=True)
+    # title = models.CharField(max_length=32, null=True, blank=True)
+    email = models.EmailField(blank=True)
+    twitter = models.CharField(max_length=15, blank=True)
+                               # current max handle length
+    facebook = models.CharField(max_length=32, blank=True)
+    mug = models.ImageField(upload_to='mug/%Y', null=True, blank=True)
+    bio = models.TextField(blank=True)
 
     def __unicode__(self):
-        "%s %s" % (self.first_name, self.last_name)
+        return "%s %s" % (self.first_name, self.last_name)
