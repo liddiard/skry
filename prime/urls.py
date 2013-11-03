0996a6422275e5f5009fe799ee020c151162ad1b
omniaura <omniaura5@gmail.com>
2013-11-03 13:51:28 -0800

    article detail view

--- prime/urls.py
+++ prime/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from prime.views import Front
+from prime.views import Front, Article
 
 urlpatterns = patterns('',
     url(r'^$', Front.as_view(), name='prime_front'),
+    url(r'^(?P<slug>[-_\w]+)/$', Article.as_view(), name='prime_article'),
 )
--- prime/views.py
+++ prime/views.py
@@ -1,5 +1,6 @@
 from prime.models import Issue, Article
 from django.views.generic import View
+from django.views.generic.detail import DetailView
 from django.shortcuts import render_to_response
 from dailybruin.settings.base import MEDIA_URL
 
@@ -9,3 +10,12 @@ class Front(View):
         articles = Article.objects.filter(issue=current_issue)
         context = {'articles': articles, 'MEDIA_URL': MEDIA_URL}
         return render_to_response('prime_front.html', context)
+
+class Article(DetailView):
+    model = Article
+    template_name = "prime_article.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(Article, self).get_context_data(**kwargs)
+        # additional context vars here
+        return context
