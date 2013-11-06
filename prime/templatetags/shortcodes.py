89eb95fb4818b75cad5b127bfdefa07042d75590
omniaura <omniaura5@gmail.com>
2013-11-06 10:45:47 -0800

    custom display param for img shortcode

--- prime/templatetags/shortcodes.py
+++ prime/templatetags/shortcodes.py
@@ -8,25 +8,31 @@ register = template.Library()
 @register.filter
 @stringfilter
 def shortcode(value):
-    img = r'\[img(\d+)\]'
+    img = r'\[img(?P<pk>\d+)\s*(?P<display>\S+)?\]'
     return re.sub(img, imgHTML, value)
 
 def imgHTML(match):
     try:
-        image = Image.objects.get(pk=match.groups()[0])
+        image = Image.objects.get(pk=match.group('pk'))
     except Image.DoesNotExist:
         return ""
-    data = {'MEDIA_URL': MEDIA_URL,
-            'filename': image.image,
-            'author': image.author,
-            'caption': image.caption
+    given_display = match.group('display')
+    # IDEA: check if given_display is in a list of valid dispaly options.
+    # If not, assign it to default value.
+    # Would prevent users from assigning any css class they want.
+    display = given_display if given_display is not None else "right"
+    params = {'display': display,
+              'MEDIA_URL': MEDIA_URL,
+              'filename': image.image,
+              'author': image.author,
+              'caption': image.caption
     }
     return '''
-           <figure>
+           <figure class="%(display)s">
                <div class="image">
                    <img src="%(MEDIA_URL)s%(filename)s"/>
                    <div class="credit">%(author)s</div>
                </div>
                <figcaption>%(caption)s</figcaption>
            </figure>
-           ''' % data
+           ''' % params
