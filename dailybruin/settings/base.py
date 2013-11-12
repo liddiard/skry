b8948d002ec4b743386abd81cb4dfe763fb4a720
DB Server <online@media.ucla.edu>
2013-11-11 22:42:11 -0800

    production database config

--- dailybruin/settings/base.py
+++ dailybruin/settings/base.py
@@ -20,6 +20,15 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dailybruin',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
