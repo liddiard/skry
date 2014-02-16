278b5d7c60f1d5cea706fd4aacd40fcf52a5c0bb
omniaura <omniaura5@gmail.com>
2014-02-15 23:21:06 -0800

    fixed import, pep 8 compliance improvements

--- prime/views.py
+++ prime/views.py
@@ -3,9 +3,12 @@ from django.views.generic import View
 from django.views.generic.detail import DetailView
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import Http404
-from dailybruin.settings.base import MEDIA_URL, STATIC_URL
+from django.conf import settings
 
-def getRecentIssues(issue_slug=None):
+
+# utility functions
+
+def get_recent_issues(issue_slug=None):
     if issue_slug is None:
         issue = Issue.objects.latest('release_date')
         recent_issues = Issue.objects.order_by('-release_date')[1:4]
@@ -18,19 +21,25 @@ def getRecentIssues(issue_slug=None):
     return issue, recent_issues
 
 
+# pages
+
 class IssueView(View):
+
     def get(self, context, slug):
-        issue, recent_issues = getRecentIssues(slug)
+        issue, recent_issues = get_recent_issues(slug)
         articles = Article.objects.filter(issue=issue).order_by('position')
         pdf = PDF.objects.get(issue=issue)
         context = {'issue': issue, 'recent_issues': recent_issues,
-                   'articles': articles, 'pdf': pdf, 'MEDIA_URL': MEDIA_URL,
-                   'STATIC_URL': STATIC_URL}
+                   'articles': articles, 'pdf': pdf, 
+                   'MEDIA_URL': settings.MEDIA_URL,
+                   'STATIC_URL': settings.STATIC_URL}
         return render_to_response('prime_front.html', context)
 
+
 class ArticleView(View):
+
     def get(self, context, issue_slug, article_slug):
-        issue, recent_issues = getRecentIssues(issue_slug)
+        issue, recent_issues = get_recent_issues(issue_slug)
         try:
             article = Article.objects.filter(issue=issue).get(slug=article_slug)
         except Article.DoesNotExist:
@@ -39,12 +48,17 @@ class ArticleView(View):
         pdf = PDF.objects.get(issue=issue)
         context = {'issue': issue, 'recent_issues': recent_issues,
                    'article': article, 'articles': articles, 'pdf': pdf,
-                   'MEDIA_URL': MEDIA_URL, 'STATIC_URL': STATIC_URL}
+                   'MEDIA_URL': settings.MEDIA_URL, 
+                   'STATIC_URL': settings.STATIC_URL}
         return render_to_response('prime_article.html', context)
 
+
+# special handlers
+
 def error404(request): # not currently implemented
-    issue, recent_issues = getRecentIssues()
+    issue, recent_issues = get_recent_issues()
     print "hello"
     context = {'issue': issue, 'recent_issues': recent_issues,
-               'MEDIA_URL': MEDIA_URL, 'STATIC_URL': STATIC_URL}
+               'MEDIA_URL': settings.MEDIA_URL, 
+               'STATIC_URL': settings.STATIC_URL}
     return render_to_response('404.html', context)
