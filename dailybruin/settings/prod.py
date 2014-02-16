43d1bc6fac34a9adabd75d7e7d69a51311cf7602
omniaura <omniaura5@gmail.com>
2014-02-15 23:44:49 -0800

    added production database settings

--- dailybruin/settings/prod.py
+++ dailybruin/settings/prod.py
@@ -1,3 +1,18 @@
 from .base import *
 
 DEBUG = False
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'dailybruin', 
+        'USER': 'dailybruin',
+        'PASSWORD': 'bruin111',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
