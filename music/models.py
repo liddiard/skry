de59cc44a525b4d811030bf53f17bbbe8a110372
Steven <shadownite7@gmail.com>
2014-05-06 19:43:00 -0700

    added model fields

--- music/models.py
+++ music/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-class Album(models.Model):
-    def __init__(self):
-        none
-
+class Album(models.Model):	
+	title = models.CharField(max_length=128)
+	artist = models.CharField(max_length=64)
+	rating = models.IntegerField()
+	review_url = models.URLField()
+	author = models.ForeignKey('main.Author')
+	artwork = models.ImageField(upload_to='music/')
+	spotify_url = models.CharField(max_length=64)
