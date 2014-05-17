c9898c0d6eee22d6bb166f42ca3c3ebdcebb616c
omniaura <omniaura5@gmail.com>
2014-05-17 01:28:18 -0700

    added Article body, models for category, media, polls, page, et al.

--- main/models.py
+++ main/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+CARD_CROP_CHOICES = (
+    ('c', 'center'), 
+    ('t', 'top/left'), 
+    ('b', 'bottom/right')
+)
+
+
 class Author(models.Model):
     user = models.ForeignKey(User, null=True, blank=True)
     first_name = models.CharField(max_length=32)
@@ -21,11 +28,6 @@ class Author(models.Model):
 
 
 class Article(models.Model):
-    CARD_CROP_CHOICES = (
-        ('c', 'center'), 
-        ('t', 'top/left'), 
-        ('b', 'bottom/right')
-    )
     STATUS_CHOICES = (
         ('dr', 'draft'),
         ('e1', 'first editing'),
@@ -43,6 +45,7 @@ class Article(models.Model):
     subhead = models.CharField(max_length=128)
     teaser = models.CharField(max_length=128)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    body = models.CharField()
     template = models.ForeignKey('Template') # TODO: add default
 
     # organization
@@ -54,7 +57,7 @@ class Article(models.Model):
     card = models.ImageField(null=True, blank=True) # TODO: missing upload_to
     card_2x = models.ImageField(null=True, blank=True)
     card_size = models.ForeignKey('CardSize')
-    card_crop = models.CharField(max_length=1, choices=CARD_CROP_CHOICES)
+    card_crop = models.CharField(max_length=1, choices=CARD_CROP_CHOICES, default='c')
     feature_card_image = models.BooleanField(default=True)
 
     # dates and times
@@ -69,3 +72,78 @@ class Article(models.Model):
     featured_audio = models.ForeignKey('Audio', null=True, blank=True)
     review = models.ForeignKey('Review', null=True, blank=True)
     # social_media_post = models.OneToOneField('scheduler.SMPost', null=True, blank=True)
+
+
+class CardSize(models.Model):
+    width = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', null=True, blank=True)
+    name = models.CharField(max_length=32, unique=True)
+    default_card = models.ImageField() # TODO: missing upload_to
+    default_card_2x = models.ImageField() # TODO: missing upload_to
+    default_card_crop = models.CharField(max_length=1, choices=CARD_CROP_CHOICES, default='c')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    slug = models.CharField(max_length=32, unique=True)
+
+
+class Template(models.Model):
+    filename = models.CharField(max_length=64, unique=True)
+    verbose_name = models.CharField(max_length=64, unique=True)
+
+
+class Page(models.Model):
+    parent = models.ForeignKey('self', null=True, blank=True)
+    title = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128)
+    template = models.ForeignKey('Template')
+    body = models.TextField()
+
+
+class Media(models.Model):
+    caption = models.TextField()
+    credit = models.ManyToManyField('Author', blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(Media):
+    image = models.ImageField() # TODO: missing upload_to
+    image_full = models.ImageField() # TODO: missing upload_to
+    image_full_2x = models.ImageField() # TODO: missing upload_to
+    image_float = models.ImageField() # TODO: missing upload_to
+    image_float_2x = models.ImageField() # TODO: missing upload_to
+
+
+class Video(Media):
+    title = models.CharField(max_length=128, blank=True, null=True)
+    url = models.URLField()
+
+
+class Audio(Media):
+    title = models.CharField(max_length=128, blank=True, null=True)
+    mp3 = models.FileField() # TODO: add upload_to
+    ogg = models.FileField() # TODO: add upload_to
+
+
+class Review(models.Model):
+    item = models.CharField(max_length=64)
+    info = models.TextField(blank=True)
+    rating = models.PositiveIntegerField(null=True, blank=True) # TODO: limit to range
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=128)
+    is_open = models.BooleanField(default=True)
+
+
+class PollChoice(models.Model)
+    question = models.ForeignKey('Question')
+    text = models.CharField(max_length=128)
+    votes = models.PositiveIntegerField(default=0)
