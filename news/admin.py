f7d81baaac654a009ba2f907524ac4ecfea90436
omniaura <omniaura5@gmail.com>
2014-05-18 17:53:37 -0700

    registered all news models in the admin

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,87 @@
 from django.contrib import admin
-from .models import Author
+
+from . import models
+
 
 class AuthorAdmin(admin.ModelAdmin):
     pass
-admin.site.register(Author)
+
+admin.site.register(models.Author, AuthorAdmin)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Article, ArticleAdmin)
+
+
+class InternalArticleCommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.InternalArticleComment, InternalArticleCommentAdmin)
+
+
+class CardSizeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.CardSize, CardSizeAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Category, CategoryAdmin)
+
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Tag, TagAdmin)
+
+
+class TemplateAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Template, TemplateAdmin)
+
+
+class PageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Page, PageAdmin)
+
+
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Image, ImageAdmin)
+
+
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Video, VideoAdmin)
+
+
+class AudioAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Audio, AudioAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Review, ReviewAdmin)
+
+
+class PollAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Poll, PollAdmin)
+
+
+class PollChoiceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.PollChoice, PollChoiceAdmin)
