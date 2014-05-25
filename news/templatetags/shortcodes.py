478b5d51b80cdb7011f9e5c14b6b61835afdbdb3
omniaura <omniaura5@gmail.com>
2014-05-24 23:16:55 -0700

    video, audio, and infobox templatetags

--- news/templatetags/shortcodes.py
+++ news/templatetags/shortcodes.py
@@ -1,63 +1,77 @@
 import re
+
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.template import Context
+from django.template.loader import get_template
+
 from project.settings.base import MEDIA_URL
-from prime.models import Image
+from news.models import Image, Video, Audio
+
 register = template.Library()
+
 
 @register.filter(is_safe=True)
 @stringfilter
 def image(value):
-    img = r'\[img(?P<pk>\d+)\s*(?P<display>\S+)?\]'
-    return re.sub(img, imgHTML, value)
-    
-@register.filter(is_safe=True)
-@stringfilter
-def youtube(value):
-    yt = r'\[youtube\]http://youtu\.be/(?P<uid>\S+)\[/youtube\]'
-    return re.sub(yt, ytHTML, value)
-
+    img_re = r'\[img(?P<pk>\d+)\s+(?P<align>\S+)?\]'
+    template = get_template('news/inc/image.html')
+    return re.sub(img_re, imgHTML, value)
 
 def imgHTML(match):
     try:
         image = Image.objects.get(pk=match.group('pk'))
     except Image.DoesNotExist:
-        return "" # IDEA: don't fail silently?
-    given_display = match.group('display')
-    # IDEA: check if given_display is in a list of valid dispaly options.
-    # If not, assign it to default value.
-    # Would prevent users from assigning any css class they want.
-    display = given_display if given_display is not None else "right"
-    params = {'display': display,
-              'MEDIA_URL': MEDIA_URL,
-              'filename': image.image,
-              'caption': image.caption
-    }
-    if image.author:
-        params['author'] = image.author
-        organization = image.author.organization
-        if organization:
-            params['organization'] = " / %s" % organization
-        else:
-            params['organization'] = ""
-    else:
-        params['organization'] = ""
-        params['author'] = ""
-    return '''
-           <figure class="%(display)s">
-               <div class="image">
-                   <img src="%(MEDIA_URL)s%(filename)s"/>
-                   <div class="credit"><strong>%(author)s</strong>
-                       %(organization)s</div>
-               </div>
-               <figcaption>%(caption)s</figcaption>
-           </figure>
-           ''' % params
+        return "" # fail silently
+    align = match.group('align')
+    template = get_template('news/inc/image.html')
+    context = Context({'image': image, 'align': align})
+    return template.render(context)
+    
 
-def ytHTML(match):
-    uid = match.group('uid')
-    return '''
-        <iframe width="500" height="281" 
-                src="//www.youtube.com/embed/%s" frameborder="0" 
-                allowfullscreen></iframe>
-           ''' % uid
+@register.filter(is_safe=True)
+@stringfilter
+def video(value):
+    vd_re = r'\[video(?P<pk>\d+)\s+(?P<align>\S+)?\]'
+    return re.sub(vd_re, vdHTML, value)
+
+def vdHTML(match):
+    try:
+        video = Video.objects.get(pk=match.group('pk'))
+    except Video.DoesNotExist:
+        return "" # fail silently
+    align = match.group('align')
+    template = get_template('news/inc/video.html')
+    context = Context({'video': video, 'align': align})
+    return template.render(context)
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def audio(value):
+    aud_re = r'\[audio(?P<pk>\d+)\s+(?P<align>\S+)?\]'
+    return re.sub(aud_re, audHTML, value)
+
+def audHTML(match):
+    try:
+        audio = Audio.objects.get(pk=match.group('pk'))
+    except Audio.DoesNotExist:
+        return "" # fail silently
+    align = match.group('align')
+    template = get_template('news/inc/audio.html')
+    context = Context({'audio': audio, 'align': align, 'MEDIA_URL': MEDIA_URL})
+    return template.render(context)
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def infobox(value):
+    inf_re = r'\[infobox\s+(?P<align>\S+)\](?P<content>.*)\[/infobox\]'
+    return re.sub(inf_re, infHTML, value, flags=re.DOTALL)
+
+def infHTML(match):
+    align = match.group('align')
+    content = match.group('content')
+    template = get_template('news/inc/infobox.html')
+    context = Context({'content': content, 'align': align})
+    return template.render(context)
