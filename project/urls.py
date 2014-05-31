1f204cee9f411d35097561b1044d197ac94e8fe4
omniaura <omniaura5@gmail.com>
2014-05-30 23:59:55 -0700

    moved category colors and article styling into sass partials, menu improvement

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # main site
-    url(r'^$', news.views.CategoryView.as_view(), name='news_category'),
+    url(r'^$', news.views.CategoryView.as_view(), name='news_front'),
     url(r'^category/(?P<category>\S+)/$', news.views.CategoryView.as_view(), name='news_category'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>\S+)/$', news.views.ArticleView.as_view(), name='news_article'),
 
