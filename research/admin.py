41f90a2548eff657674b8806ea1d7b2d82a090a7
omniaura <omniaura5@gmail.com>
2015-02-23 16:59:56 -0800

    registered models in admin

--- research/admin.py
+++ research/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(models.Post, PostAdmin)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(models.Project, ProjectAdmin)
+
+
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Image, ImageAdmin)
