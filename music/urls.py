32868b06d72f078a794cbaa55a5c4fc43c34dc4b
omniaura <omniaura5@gmail.com>
2015-04-13 16:34:25 -0700

    deleted music, research, and prime apps
    these apps will be merged into the final CMS, which will now be split into multiple apps. go big or go home.

--- music/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from .views import MainView
-
-urlpatterns = patterns('',
-    url(r'^$', MainView.as_view(), name='main_page')
-)
-
--- research/utils.py
+++
@@ -1,6 +0,0 @@
-def pretty_list_from_queryset(items):
-    if len(items) < 2:
-        return str(items.first())
-    else:
-        return ", ".join([str(i) for i in items if i != items.last()]) + \
-               " and " + str(items.last())
