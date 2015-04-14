7a46e0ae0986c0a34c4ee4c994d1f2eae8154874
omniaura <omniaura5@gmail.com>
2015-04-14 14:06:33 -0700

    added filter and search fields to StoryViewSet

--- core/api.py
+++ core/api.py
@@ -7,4 +7,7 @@ from . import models
 class StoryViewSet(viewsets.ModelViewSet):
     queryset = models.Story.objects.all()
     serializer_class = serializers.StorySerializer
-    filter_fields = ('position',)
+    filter_fields = ('status', 'author', 'position', 'sections', 'tags',
+                     'sites')
+    search_fields = ('title', 'teaser', 'subhead', 'body')
+    ordering_fields = "__all__"
