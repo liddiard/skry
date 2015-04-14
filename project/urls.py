5a0f2f42d2966e5ab8728bef56edecefe842e93b
omniaura <omniaura5@gmail.com>
2015-04-14 13:27:51 -0700

    readded admin urls to urlconf

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 from rest_framework import routers
 
 from access import api as access_api
@@ -11,7 +12,8 @@ router.register(r'groups', access_api.GroupViewSet)
 router.register(r'stories', core_api.StoryViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework'))
+                               namespace='rest_framework')),
+    url(r'^', include(router.urls)) # main api
 ]
