df1afae28655e283c31bd23318b1266414b71fb5
omniaura <omniaura5@gmail.com>
2015-04-14 14:59:41 -0700

    added remainder of core serializers and views

--- core/serializers.py
+++ core/serializers.py
@@ -3,7 +3,23 @@ from rest_framework import serializers
 from . import models
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Author
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Status
+
+
 class StorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Story
+        depth = 1
+
+
+class PageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Page
         depth = 1
