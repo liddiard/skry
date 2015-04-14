5c29744aaef97502849e4e4e4605477b13047bd0
Harrison Liddiard <omniaura5@gmail.com>
2015-04-14 16:44:26 -0700

    added filter, search, ordering fields on core viewsets

--- access/views.py
+++ access/views.py
@@ -7,8 +7,16 @@ from . import serializers
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+    filter_fields = ('groups', 'user_permissions', 'is_staff', 'is_active',
+                     'is_superuser')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering_fields = ('username', 'first_name', 'last_name', 'email',
+                       'last_login', 'date_joined')
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = serializers.GroupSerializer
+    filter_fields = ('permissions',)
+    search_fields = ('name',)
+    ordering_fields = ('name',)
