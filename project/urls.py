0414a5bbb2b0afc28f8634bf5f972d8bec328edc
omniaura <omniaura5@gmail.com>
2015-04-13 18:46:51 -0700

    renamed local 'auth' app to 'access'
    because apparently django doesn't like that name since it conflicts with django.contrib.auth

--- project/urls.py
+++ project/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
-from auth import api as auth_api
+from access import api as access_api
 from core import api as core_api
 
 
 router = routers.DefaultRouter()
-router.register(r'users', auth_api.UserViewSet)
-router.register(r'groups', auth_api.GroupViewSet)
+router.register(r'users', access_api.UserViewSet)
+router.register(r'groups', access_api.GroupViewSet)
 router.register(r'stories', core_api.StoryViewSet)
 
 urlpatterns = [
