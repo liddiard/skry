09857a836b25b5e7520608f1b4ab0fb3cdd0852a
omniaura <omniaura5@gmail.com>
2015-04-14 15:20:52 -0700

    change api permissions to DjangoModelPermissions

--- project/settings/base.py
+++ project/settings/base.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.DjangoModelPermissions'
     ],
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.DjangoFilterBackend',
