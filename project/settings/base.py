1acbe6d587025eb0cc39d68ca1c7ab274c309189
omniaura <omniaura5@gmail.com>
2015-04-14 11:15:27 -0700

    readded MIDDLEWARE_CLASSES b/c apparently django needs that

--- project/settings/base.py
+++ project/settings/base.py
@@ -57,6 +57,15 @@ ROOT_URLCONF = 'project.urls'
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
