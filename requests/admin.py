cbaf270f9e9c15e89c3d6446af99b5c5922b619a
omniaura <omniaura5@gmail.com>
2015-04-15 13:34:19 -0700

    can't display a ManyToManyField on an admin list_display

--- requests/admin.py
+++ requests/admin.py
@@ -4,21 +4,21 @@ from . import models
 
 
 class PhotoRequestAdmin(admin.ModelAdmin):
-    list_display = ['story', 'assignees', 'location', 'time']
+    list_display = ['story', 'location', 'time']
 
 
 admin.site.register(models.PhotoRequest, PhotoRequestAdmin)
 
 
 class GraphicRequestAdmin(admin.ModelAdmin):
-    list_display = ['story', 'assignees']
+    list_display = ['story']
 
 
 admin.site.register(models.GraphicRequest, GraphicRequestAdmin)
 
 
 class IllustrationRequestAdmin(admin.ModelAdmin):
-    list_display = ['story', 'assignees']
+    list_display = ['story']
 
 
 admin.site.register(models.IllustrationRequest, IllustrationRequestAdmin)
