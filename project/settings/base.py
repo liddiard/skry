032552016416df8b2dcd797b20b36c4794a5a383
omniaura <omniaura5@gmail.com>
2015-04-15 16:21:14 -0700

    allowing session auth
    for ease of use with Postman development for now

--- project/settings/base.py
+++ project/settings/base.py
@@ -120,6 +120,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.DjangoFilterBackend',
