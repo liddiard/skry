ea28a6c141c47a2f3461b2b4fd0423a4cb4d3eb4
omniaura <omniaura5@gmail.com>
2015-04-17 17:01:35 -0700

    display viewsets

---
+++ display/views.py
@@ -0,0 +1,36 @@
+from rest_framework import viewsets
+
+from . import serializers
+from . import models
+
+
+class CardSizeViewSet(viewsets.ModelViewSet):
+    queryset = models.CardSize.objects.all()
+    serializer_class = serializers.CardSizeSerializer
+    filter_fields = ('width', 'height')
+    search_fields = ()
+    ordering_fields = "__all__"
+
+
+class StylesheetViewSet(viewsets.ModelViewSet):
+    queryset = models.Stylesheet.objects.all()
+    serializer_class = serializers.StylesheetSerializer
+    filter_fields = ()
+    search_fields = ('name', 'file')
+    ordering_fields = "__all__"
+
+
+class ScriptViewSet(viewsets.ModelViewSet):
+    queryset = models.Script.objects.all()
+    serializer_class = serializers.ScriptSerializer
+    filter_fields = ()
+    search_fields = ('name', 'file')
+    ordering_fields = "__all__"
+
+
+class TemplateViewSet(viewsets.ModelViewSet):
+    queryset = models.Template.objects.all()
+    serializer_class = serializers.TemplateSerializer
+    filter_fields = ('stylesheets', 'scripts')
+    search_fields = ('name', 'file')
+    ordering_fields = "__all__"
