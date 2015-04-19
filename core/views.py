ed4f49ab2899d86476050ec25fe9157815437fd2
omniaura <omniaura5@gmail.com>
2015-04-18 18:21:02 -0700

    fixed reference to field name

--- core/views.py
+++ core/views.py
@@ -15,7 +15,7 @@ class StatusViewSet(viewsets.ModelViewSet):
 class StoryViewSet(viewsets.ModelViewSet):
     queryset = models.Story.objects.all()
     serializer_class = serializers.StorySerializer
-    filter_fields = ('status', 'author', 'position', 'sections', 'tags',
+    filter_fields = ('status', 'authors', 'position', 'sections', 'tags',
                      'sites')
     search_fields = ('title', 'teaser', 'subhead', 'body')
     ordering_fields = "__all__"
