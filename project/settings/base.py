e4dd2d8c6f62b6f0045039f121cb713915d62ed8
omniaura <omniaura5@gmail.com>
2015-04-19 22:52:22 -0700

    removed api SessionAuthentication

--- project/settings/base.py
+++ project/settings/base.py
@@ -121,7 +121,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.DjangoFilterBackend',
