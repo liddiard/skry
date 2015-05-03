2074a0956b3d4c2b8a435d96ae97d5b88d4a7771
omniaura <omniaura5@gmail.com>
2015-05-02 17:36:18 -0700

    serialize URL of ArtRequest associated w/ an Image
    makes use of some less-obvious functionality of Django Rest Framework's generated URL patterns as well as Django's content types framework, so it's heavily commented

--- attachments/serializers.py
+++ attachments/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+from django.contrib.contenttypes.models import ContentType
 
 from authors import serializers as authors_serializers
 from . import models
@@ -10,6 +12,7 @@ class MediaSerializer(serializers.HyperlinkedModelSerializer):
 
 class ImageSerializer(MediaSerializer):
     resized = serializers.SerializerMethodField()
+    request = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Image
@@ -24,6 +27,35 @@ class ImageSerializer(MediaSerializer):
         elif resolution:
             return obj.get_image_at_resolution(resolution)
         else:
+            return None
+
+    def get_request(self, obj):
+        # check if the Image has both components for a GenericForeignKey
+        if obj.content_type and obj.object_id:
+
+            # get the model of the request type that the Image points to
+            art_request_model = obj.content_type.model_class()
+
+            # query for the object using the appropriate model type
+            art_request_obj = art_request_model.objects.get(id=obj.object_id)
+
+            # construct the URL name to reverse which matches Django Rest
+            # Framework's generated pattern, a combination of namespace
+            # (https://docs.djangoproject.com/en/1.8/topics/http/urls/#topics-http-reversing-url-namespaces),
+            # content type model
+            # (https://docs.djangoproject.com/en/1.8/ref/contrib/contenttypes/#the-contenttype-model),
+            # and the view type, 'detail', which Django Rest Framework uses
+            # to specify a detail view versus a list view
+            # (http://www.django-rest-framework.org/api-guide/routers/#usage)
+            detail_url = "v1:%s-detail" % obj.content_type.model
+
+            # return the absolute url of the associated art request, using
+            # Django Rest Framework's own reverse() function
+            # (http://www.django-rest-framework.org/api-guide/reverse/#reverse)
+            return reverse(detail_url, kwargs={'pk': art_request_obj.pk},
+                           request=self.context['request'])
+
+        else: # this Image isn't linked to a request
             return None
 
 
