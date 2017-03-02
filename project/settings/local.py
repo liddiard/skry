d833510a0817f2e813f6d5b2af9d2dc9ee0b817d
Harrison Liddiard <liddiard@users.noreply.github.com>
2017-03-02 15:37:30 -0800

    use a dummy SECRET_KEY for local dev

--- project/settings/local.py
+++ project/settings/local.py
@@ -5,6 +5,10 @@ THUMBNAIL_DEBUG = True
 
 STATIC_ROOT = BASE_DIR + "/static"
 
+# WARNING: do not run with local settings in production. The secret
+# key below is a dummy value for use only when you don't care about 
+# authentication security (i.e. developing on your local machine).
+SECRET_KEY = "unsecure-dummy-development"
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
